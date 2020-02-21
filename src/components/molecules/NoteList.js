0a376457247b6d7463d38675d113e0e17f40bf55
ssparvez <shhrrparvez@gmail.com>
2020-02-20 23:54:22 -0500

    restructured note list

--- src/components/molecules/NoteList.js
+++ src/components/molecules/NoteList.js
@@ -1,32 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import './NoteList.scss';
-import { copyToClipboard, isValidURL } from '../../utils';
 import Loader from '../atoms/Loader';
-import _ from 'lodash';
-import history from '../../history';
 
 import noNotesImage from '../../assets/undraw_no_data_qbuo.svg'; // Tell Webpack this JS file uses this image
 import EmptyState from '../atoms/EmptyState';
 
-import ReactTooltip from 'react-tooltip';
 import { withToast } from 'react-awesome-toasts';
-import Modal from '../molecules/Modal';
 
 import { connect } from 'react-redux';
-import { createNote, fetchNotes, deleteNote, fetchTags, editNote } from '../../actions';
+import { createNote, fetchNotes, fetchTags } from '../../actions';
 import JotBar from '../atoms/JotBar';
+import NoteListItem from './NoteListItem';
 
 class NoteList extends Component {
   constructor(props) {
     super(props);
 
     this.messagesEndRef = React.createRef();
-
-    this.state = {
-      showTagPicker: false,
-      selectedNote: null,
-      noteWasSubmitted: false
-    }
   }
 
   componentDidMount() {
@@ -46,7 +36,6 @@ class NoteList extends Component {
 
   componentDidUpdate() {
     this.scrollToBottom();
-    // this.setState({ noteWasSubmitted: false });
   }
 
   onSubmit = (noteContent) => { // jotbar isnt in this component
@@ -65,7 +54,6 @@ class NoteList extends Component {
         <div className="note-list">
           {this.renderNoteList()}
           <div style={{ float:"left", clear: "both" }} ref={this.messagesEndRef} />
-          { this.state.showTagPicker && this.renderTagSelector() }
           <JotBar onSubmit={this.onSubmit} />
         </div>
       )
@@ -79,27 +67,13 @@ class NoteList extends Component {
           <EmptyState image={noNotesImage} label={label} />
         )
       }
-
     }
   }
 
   renderNoteList() {
     let previousDate = new Date('1970-01-01');
-    return this.props.notes.map(({ content, _id, createdOn, tagId }) => {
-      // set tag options
-      let tag;
-      if (tagId && _.size(this.props.tags) > 0 && this.props.tags[tagId]) {
-        tag = {
-          name: this.props.tags[tagId].name,
-          color: this.props.tags[tagId].color
-        }
-      }
-
-      const timeOptions = { hour: 'numeric', minute: 'numeric' };
-
-      let currentDate = new Date(createdOn);
-      let time = currentDate.toLocaleTimeString("en-US", timeOptions); // Saturday, September 17, 2016
-
+    return this.props.notes.map(note => {
+      let currentDate = new Date(note.createdOn);
       currentDate.setHours(0, 0, 0, 0);
 
       let dateHeader;
@@ -111,61 +85,18 @@ class NoteList extends Component {
       previousDate = currentDate;
 
       return (
-        <Fragment key={_id}>
+        <Fragment key={note._id}>
           { dateHeader && <div className="date">{dateHeader}</div>}
-          <div className="item" >
-            <div className="details">
-              <div className="time">{time}</div>
-              { isValidURL(content) ? 
-                <div className="message link" onClick={() => window.open(content.includes('http') ? content : 'http://' + content, '_blank') } data-tip="Go to site">{content}</div> : 
-                <div className="message" data-tip="Copy to clipboard" onClick={() => copyToClipboard(content, this.props.toast)}>{content}</div>
-              }
-              { tag && <div className="tag" style={{color: tag.color}} onClick={() => history.push(`/tags/${tagId}`)}>#{tag.name}</div> }
-            </div>
-            
-            <div className="actions">
-              { !tag ?
-                <i className="material-icons add-tag" data-tip="Add Tag" onClick={() => this.setState({showTagPicker: true, selectedNote: _id})}>label</i> :
-                <i className="material-icons remove-tag" data-tip="Remove Tag" onClick={() => this.removeTagFromNote(_id)}>label_off</i>
-              }
-              <i className="material-icons edit" data-tip="Edit">edit</i>
-              <i className="material-icons delete" data-tip="Remove" onClick={() => this.props.deleteNote(_id)}>delete</i>
-            </div>
-            <ReactTooltip />
-          </div>
+          <NoteListItem previousDate={previousDate} note={note} tags={this.props.tags} />
         </Fragment>
-      );
+      )
     })
-  }
-
-  renderTagList() {
-    return _.map(this.props.tags, ({ color, name, _id }) => {
-      return <div key={_id} className="tag" style={{backgroundColor: color}} onClick={() => this.assignTagToNote(_id)}>{name}</div>;
-    });
-  }
-
-  assignTagToNote(tagId) {
-    this.setState({ showTagPicker: false, selectedNote: null })
-    this.props.editNote(this.state.selectedNote, { tagId })
-  }
-
-  removeTagFromNote = (noteId) => {
-    this.props.editNote(noteId, { tagId: null });
   }
 
   scrollToBottom = () => {
     if (this.props.notes.length > 0) {
       this.messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }
-
-  renderTagSelector() {
-    return (
-      <Modal id="tag-selector" onDismiss={() => this.setState({showTagPicker: false})}>
-        <div className="header">Select a tag</div>
-        <div className="content">{this.renderTagList()}</div>
-      </Modal>
-    )
   }
 }
 
@@ -180,4 +111,4 @@ const mapStateToProps = (state) => {
 }
 
 
-export default connect(mapStateToProps, { createNote, fetchNotes, deleteNote, fetchTags, editNote })(withToast(NoteList));
+export default connect(mapStateToProps, { createNote, fetchNotes, fetchTags })(withToast(NoteList));
---
+++ src/components/molecules/NoteListItem.js
@@ -0,0 +1,133 @@
+import React, { Component, Fragment } from 'react';
+import { copyToClipboard, isValidURL } from '../../utils';
+import history from '../../history';
+import ReactTooltip from 'react-tooltip';
+import _ from 'lodash';
+import { deleteNote, editNote } from '../../actions';
+import { connect } from 'react-redux';
+import { withToast } from 'react-awesome-toasts';
+import './NoteListItem.scss';
+import Modal from '../molecules/Modal';
+
+
+class NoteListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.updateButtonRef = React.createRef();
+
+
+    this.state = {
+      isEditMode: false,
+      content: this.props.note.content,
+      showTagPicker: false,
+    }
+  }
+
+  renderContent(content) {
+
+    if (!this.state.isEditMode) {
+      if (isValidURL(content)) {
+        return <div className="message link" onClick={() => window.open(content.includes('http') ? content : 'http://' + content, '_blank') } data-tip="Go to site">{content}</div>;
+      } else {
+        return <div className="message" data-tip="Copy to clipboard" onClick={() => copyToClipboard(content, this.props.toast)}>{content}</div>;
+
+      }
+    } else {
+      return (
+        <Fragment>
+          <div className="mask"></div>
+          <input autoFocus value={this.state.content} onChange={this.handleChange} onKeyPress={this.keyPressed} onBlur={this.handleBlur} />
+          <button className="filled blue" onClick={this.updateNote} ref={this.updateButtonRef}>Update</button>
+        </Fragment>
+      )
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ content: event.target.value });
+  }
+
+  handleBlur = (event) => {
+    if (event.relatedTarget !== this.updateButtonRef.current) {
+      this.setState({ isEditMode: false, content: this.props.note.content });
+    }
+  }
+
+  keyPressed = (event) => {
+    // console.log(event.shiftKey);
+    if (event.key === 'Enter') {
+      this.updateNote();
+    }
+  }
+
+  updateNote() {
+    this.setState({ isEditMode: false });
+    this.props.editNote(this.props.note._id, { content: this.state.content });
+  }
+
+  render() {
+    const { content, _id, createdOn, tagId } = this.props.note;
+
+    // set tag options
+    let tag;
+    if (tagId && _.size(this.props.tags) > 0 && this.props.tags[tagId]) {
+      tag = {
+        name: this.props.tags[tagId].name,
+        color: this.props.tags[tagId].color
+      }
+    }
+
+    const timeOptions = { hour: 'numeric', minute: 'numeric' };
+
+    let currentDate = new Date(createdOn);
+    let time = currentDate.toLocaleTimeString("en-US", timeOptions); // Saturday, September 17, 2016
+    return (
+      <div className="item" >
+        <div className="details">
+          <div className="time">{time}</div>
+          {this.renderContent(content)}
+          { tag && <div className="tag" style={{color: tag.color}} onClick={() => history.push(`/tags/${tagId}`)}>#{tag.name}</div> }
+        </div>
+        
+        <div className="actions">
+          { !tag ?
+            <i className="material-icons add-tag" data-tip="Add Tag" onClick={() => this.setState({showTagPicker: true})}>label</i> :
+            <i className="material-icons remove-tag" data-tip="Remove Tag" onClick={() => this.removeTagFromNote(_id)}>label_off</i>
+          }
+          <i className="material-icons edit" data-tip="Edit" onClick={() => this.setState({ isEditMode: true })}>edit</i>
+          <i className="material-icons delete" data-tip="Remove" onClick={() => this.props.deleteNote(_id)}>delete</i>
+        </div>
+        { this.state.showTagPicker && this.renderTagSelector() }
+
+        <ReactTooltip />
+      </div>
+    );
+  }
+
+  removeTagFromNote = (noteId) => {
+    this.props.editNote(noteId, { tagId: null });
+  }
+
+  assignTagToNote(tagId) {
+    this.setState({ showTagPicker: false })
+    this.props.editNote(this.props.note._id, { tagId })
+  }
+
+  renderTagSelector() {
+    return (
+      <Modal id="tag-selector" onDismiss={() => this.setState({showTagPicker: false})}>
+        <div className="header">Select a tag</div>
+        <div className="content">{this.renderTagList()}</div>
+      </Modal>
+    )
+  }
+
+  renderTagList() {
+    return _.map(this.props.tags, ({ color, name, _id }) => {
+      return <div key={_id} className="tag" style={{backgroundColor: color}} onClick={() => this.assignTagToNote(_id)}>{name}</div>;
+    });
+  }
+}
+
+export default connect(null, { deleteNote, editNote })(withToast(NoteListItem));
